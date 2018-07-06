48f4f7ee342fa0942199e64d585955b31c7de6c1
luchenghao <834651203@qq.com>
2018-07-06 17:32:52 +0800

    fix

--- app/scripts/controllers/downloadAppCtrl.js
+++ app/scripts/controllers/downloadAppCtrl.js
@@ -5,10 +5,18 @@ var app = angular.module('gfbApp');
  * 有奖-主页面 controller
  */
 app.controller('downloadAppCtrl', function($scope) {
+	var clipboard = new Clipboard('.copyBtn').on('success', function(e) {
+        var e = e || window.event;
+        console.log(e);
+    }).on('error', function(e) {});
 
     //跳转到安家趣花安卓下载页面
-    $scope.routeajppAndroid = function(){
-        window.location.href ="http://a.app.qq.com/o/simple.jsp?pkgname=com.vcredit.ajqh";  
-    }  	
+    // $scope.routeajppAndroid = function(){
+    //     window.location.href ="http://a.app.qq.com/o/simple.jsp?pkgname=com.vcredit.ajqh";  
+    // } 
+
+
+
+
     
 });
