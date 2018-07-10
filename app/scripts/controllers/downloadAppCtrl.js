9f3b06c067c344aebe1f3cfb957c0d95f6c89895
luchenghao <834651203@qq.com>
2018-07-10 18:31:18 +0800

    fix

--- app/scripts/controllers/downloadAppCtrl.js
+++ app/scripts/controllers/downloadAppCtrl.js
@@ -4,10 +4,11 @@ var app = angular.module('gfbApp');
 /**
  * 有奖-主页面 controller
  */
-app.controller('downloadAppCtrl', function($scope) {
+app.controller('downloadAppCtrl', function($scope,ngUtils) {
 	var clipboard = new Clipboard('.copyBtn').on('success', function(e) {
         var e = e || window.event;
         console.log(e);
+        ngUtils.alert('复制成功');
     }).on('error', function(e) {});
 
     //跳转到安家趣花安卓下载页面
