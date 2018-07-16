dc762ddd9d6357684b2fb6216635a14f0d535b2e
luchenghao <834651203@qq.com>
2018-07-16 15:45:16 +0800

    修改报表首页样式

--- app/scripts/controllers/operationAnalysisCtrl.js
+++ app/scripts/controllers/operationAnalysisCtrl.js
@@ -7,10 +7,11 @@ var app = angular.module('gfbApp');
 app.controller('operationAnalysisCtrl', function($scope,$routeParams) {
     //跳转到房易贷报表
     $scope.routeaFydBill = function(){
-        window.appModel.showNewWebPage("https://report-uat.apass.cn/#/fyd-home?token="+$routeParams.token,"房易贷");
+        window.appModel.showNewWebPage("https://report-uat.apass.cn/#/fyd-home?token="+$routeParams.token,"房易贷",true);
     } 
     //跳转到安家趣花报表
     $scope.routeaAjqhBill = function(){
-        window.appModel.showNewWebPage("https://report-uat.apass.cn/#/ajqh-home?token="+$routeParams.token,"安家趣花");  
+    	window.appModel.showNewWebPage("http://10.254.20.16:3000/#/ajqh-home?token="+$routeParams.token,"安家趣花",true);  
+        // window.appModel.showNewWebPage("https://report-uat.apass.cn/#/ajqh-home?token="+$routeParams.token,"安家趣花");  
     }     
 });
