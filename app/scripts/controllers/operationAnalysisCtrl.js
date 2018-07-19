31dee2e6f605f074f51fc2d667d9b481c8fd1cc0
luchenghao <834651203@qq.com>
2018-07-19 15:23:24 +0800

    修改安家趣花首页

--- app/scripts/controllers/operationAnalysisCtrl.js
+++ app/scripts/controllers/operationAnalysisCtrl.js
@@ -12,6 +12,6 @@ app.controller('operationAnalysisCtrl', function($scope,$routeParams) {
     //跳转到安家趣花报表
     $scope.routeaAjqhBill = function(){
     	// window.appModel.showNewWebPage("https://report-uat.apass.cn/#/ajqh-home?token="+$routeParams.token,"安家趣花",true);  
-        window.appModel.showNewWebPage("https://report-uat.apass.cn/#/ajqh-home?token="+$routeParams.token,"安家趣花");  
+        window.appModel.showNewWebPage("https://report-uat.apass.cn/#/ajqh-home?token="+$routeParams.token,"安家趣花",true);  
     }     
 });
